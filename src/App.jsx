5a0dc8e7192c8b2317839bb2784343a8bc5685da
umair <umairjutt2025@gmail.com>
2023-09-27 21:17:12 +0500

    Signup Remove

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import MainPage from "./Components/Main_Page/MainPage"
 import Properties from "./Components/Properties/Properties"
 import Home from "./Components/Home/Home"
 import Aboutus from "./Components/About_Us/Aboutus"
+import Contact from './Components/Contact/Contact';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path='/home' element={<Home />} />
         <Route path='/properties' element={<Properties />} />
         <Route path='/aboutus' element={<Aboutus />} />
+        <Route path='/contact' element={<Contact/>}/>
         
       </Routes>
    </>
---
+++ src/Components/Contact/Contactdata.jsx
@@ -0,0 +1,22 @@
+const datacontact= [
+    {
+
+
+    icon:"fa-solid fa-envelope",
+    info:"info@deleonrealty.com",
+
+},
+
+{
+
+
+    icon:"fa-solid fa-envelope",
+    info:"info@deleonrealty.com",
+
+},
+
+
+
+]
+
+export default datacontact
